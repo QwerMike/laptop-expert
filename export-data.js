c8918dff80e9cd798042a5ed99e812d71fed0508
Michael <tmikent@gmail.com>
2017-12-20 02:31:06 +0200

    Add function to convert data for Prolog

--- export-data.js
+++ export-data.js
@@ -1,10 +1,10 @@
-const DESCRIPTION_RGX = new RegExp(/Екран +(\d\d\.\d).+(Intel|Amd) [^/]+\(.+\).+RAM (\d+) ГБ.+(?:HDD|SSD) (\d+) (?:ГБ|ТБ)/i);
+const DESCRIPTION_RGX = new RegExp(/Екран +(\d+).+(Intel|Amd) [^/]+\(.+\).+RAM (\d+) ГБ.+(?:HDD|SSD) (\d+) (?:ГБ|ТБ)/i);
 
 let nodes = document.querySelectorAll('div[data-view_type="catalog_with_hover"]');
 let result = Array.from(nodes, 
     x => {
-        let price = x.querySelector('div.g-price-uah').innerText
-          , link = x.querySelector('div.g-i-tile-i-title.clearfix a').href
+        let price       = x.querySelector('div.g-price-uah').innerText
+          , link        = x.querySelector('div.g-i-tile-i-title.clearfix a').href
           , description = DESCRIPTION_RGX.exec(x.querySelector('div.g-i-tile-desc-wrap div.short-description').innerText);
         
         price = price.split(/\s/).slice(0, 2).join('');
@@ -16,3 +16,15 @@ let result = Array.from(nodes,
         return { link, price, display, cpu, ram, drive };
     }
 );
+
+/**
+ * Converts array of laptops to Prolog predicates
+ * @param {Array} laptops -- Array of { link, price, display, cpu, ram, drive }
+ * @returns {String} -- Prolog predicates
+ */
+function exportToProlog(laptops) {
+  return laptops.map(x => "laptop("
+                        + [x.display, x.cpu, x.ram, x.drive, x.price].join(', ')
+                        + ",\n    '" + x.link + "').")
+                .join('\n');
+}
